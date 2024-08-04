94223c853372eb0e17843658718776aa573fac19
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2024-08-05 01:59:08 +0400

    In the HTML file add tags for error messages. In the CSS file add styles for the error message and error border. In the JavaScript file add functions that check if inputs are filled correctly.

--- interactive-card-details.js
+++ interactive-card-details.js
@@ -10,15 +10,21 @@ const yearInput = document.querySelector(`#yy`);
 const cvcInput = document.querySelector(`#cvc-input`);
 
 
+
+
 const cardholderNameCard = document.querySelector(`.cardholder-name-on-card`)
 const cardNumberCard = document.querySelector(`.card-number-on-card`);
 const expdateCard = document.querySelector(`.expdate-on-card`);
 const cvcvCard = document.querySelector(`.card-cvc`);
 
 const inputsArray = Array.from(document.getElementsByClassName(`input`));
+const errorTextArray = Array.from(document.getElementsByClassName(`error-text`));
+
+
+const arrMonthes = [ `01`, `02`, `03`, `04`, `05`, `06`, `07`, `08`, `09`, `10`, `11`, `12`]
+
+
 console.log(inputsArray)
-
-
 
 
 
@@ -56,11 +62,96 @@ cvcInput.addEventListener(`input`, () => {
 });
 
 
-submitBtn.addEventListener(`click`, (e) => {
-    e.preventDefault();
 
-    if(inputsArray.some(el => el.value === ``)) {
-        console.log(`hi`)
+
+/// this function checkes if input is filled
+
+const fillInput = function(inputArr) {
+    inputArr.forEach((el, i) => {
+        if(el.value === ``) {
+            errorTextArray[i].classList.add(`visible`)
+            el.classList.add(`error-border`)
+        } else {
+            errorTextArray[i]?.classList.remove(`visible`)
+            el.classList.remove(`error-border`)   
+            
+            
+            // checkInputLeangth(cardNumberInput)
+
+        }
+    })
+}
+
+
+
+
+//// this function checks if input has required number of letters
+
+const checkInputLeangth = function(input) {
+
+    const indexOfElement = inputsArray.findIndex((el, i) => {
+       if( el.id === `${input.id}`) {
+        return i
+       }
+    })
+
+    if(input.value.length < input.maxLength) {
+        input.classList.add(`error-border`)
+        errorTextArray[indexOfElement].classList.add(`visible`)
+    } else {
+        input.classList.remove(`error-border`)
+        errorTextArray[indexOfElement].classList.remove(`visible`)
+       
+    }
+}
+
+
+
+///  this function checkes if month input is filled correctly
+
+const checkMonthInp = function(month) {
+    const x = arrMonthes.some(el => {
+        return month.value === el
+    })
+
+    const indexOfElement = inputsArray.findIndex((el, i) => {
+        if( el.id === `${month.id}`) {
+         return i
+        }
+     }) 
+
+    if(x !== true) {
+        month.classList.add(`error-border`)
+        errorTextArray[indexOfElement].classList.add(`visible`)
+    } else {
+        month.classList.remove(`error-border`)
+        errorTextArray[indexOfElement].classList.remove(`visible`)
+    }
+}
+
+
+
+
+
+
+
+submitBtn.addEventListener(`click`, (e) => {
+
+    e.preventDefault();
+    
+    fillInput(inputsArray);
+
+    checkInputLeangth(cardNumberInput);
+    checkInputLeangth(cvcInput);
+    checkInputLeangth(yearInput);
+    checkInputLeangth(monthInput);
+
+    checkMonthInp(monthInput)
+
+
+
+    if(inputsArray.some(el => el.classList.contains(`error-border`))) {
+
     } else {
         cardholderNameCard.textContent = `${cardholderNameInput.value}`;
         cardNumberCard.textContent = `${cardNumberInput.value}`;
